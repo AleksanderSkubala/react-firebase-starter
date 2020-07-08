9fb9dcb166782689a79fccac337a473dd58f272f
AleksanderSkubala <alexskubala@gmail.com>
2020-07-08 21:14:29 +0200

    clear up the projects and write base provider

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledH1 = styled.h1`
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <StyledH1>Hi user, check out login auth.</StyledH1>
+    </div>
+  );
+}
+
+export default App;
